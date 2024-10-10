4a32b66dda49de8de62e16979385aa1efca2ddb5
anayib <nayib.abdala@gmail.com>
2024-10-10 11:48:03 +0200

    Add auth services
    Add login, log out and authorization validation logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,59 @@
+import { useState } from 'react';
+import {login, isAuthenticated} from '../services/authService';
+import { useNavigate } from 'react-router-dom';
+
 function Login() {
-    console.log("Login")
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate  = useNavigate();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    
+    try {
+      const isAuthorized = await login({ userName, password });
+      if (isAuthorized) {
+        navigate('/dashboard');
+      } else {
+        alert('User not authorized. Please try again.');
+      }
+    } catch (error) {
+      console.error('Login error:', error.message);
+      alert('An error occurred during login. Please try again.');
+    }
+  }
+  
+
+  return (
+    <>
+    <h2>Login</h2>
+    <div>
+      <form onSubmit={handleLogin}>
+        <input 
+         type="text" 
+         value= {userName}
+         placeholder='user name'
+         className='relative mb-4'
+         onChange={(e) => {setUserName(e.target.value)}}
+         required
+         />
+        <input 
+        type="text" 
+        value={password}
+        placeholder='password'
+        className='relative mb-4'
+        onChange={(e) => {setPassword(e.target.value)}}
+        required
+        />
+        <button type="submit">Login</button>
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+    </>
+
+  );
+
 }
 
 export default Login;
---
+++ src/services/authService.js
@@ -0,0 +1,37 @@
+// This is where the actual login logic resides. 
+// It contains functions that handle authentication-related API calls 
+// (e.g., login, logout, token storage), and may also handle token validation
+
+async function login({ userName, password }) {
+  console.log('INSIDE LOGIN', userName, password);
+  
+  try {
+    const validCredentials = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(userName === 'Saul' && password === '123');
+      }, 2000);
+    });
+
+    console.log('INSIDE LOGIN AUTHO', validCredentials);
+    
+    if (validCredentials) {
+      localStorage.setItem('authToken', 'asfas58934hnsdf');
+    }
+
+    return validCredentials; // Return the validity directly
+  } catch (error) {
+    console.error('Error during login:', error.message); // More descriptive error message
+    throw error; // Rethrow the error for further handling
+  }
+}
+
+
+const logout = () => {
+  localStorage.removeItem('authToken');
+};
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem('authToken')
+}
+
+export { login, logout, isAuthenticated }
