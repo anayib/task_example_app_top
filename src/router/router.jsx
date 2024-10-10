61e5ea5d418e4615388c56b0df373b555d320d6f
anayib <nayib.abdala@gmail.com>
2024-10-10 10:32:59 +0200

    Add protected route component
    The protected route wrapp compknent wrapps the component to be protected, in this case the Dashboard

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,13 +3,14 @@ import Home from '../components/Home';
 import Login from '../components/Login';
 import Dashboard from '../components/Dashboard'
 import taskLoader from '../loaders/taskLoader'
+import ProtectedRoute from "./ProtectedRoute";
 
 const router = createBrowserRouter([
     {path: "/", element: < Home />},
     {path: "/login", element: <Login />},
     {
       path: "/dashboard",
-      element: <Dashboard />,
+      element:  <ProtectedRoute element={<Dashboard />} />,
       loader: taskLoader,
     }
   ]);
